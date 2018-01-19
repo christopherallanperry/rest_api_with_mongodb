4a6e9a91a042f0df705ade6483613e8aa57d6be7
chrisofarabia <chrisofarabia@yahoo.co.uk>
2018-01-19 22:10:45 +0000

    Built out model/controller files - tested with CURL statements successfully

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const logger = require("morgan");
-const errorhandler = require("errorhandler");
-const bodyParser = require("body-parser");
+const express       =  require("express");
+const logger        =  require("morgan");
+const errorhandler  =  require("errorhandler");
+const bodyParser    =  require("body-parser");
+const mongoose      =  require("mongoose");
+const app           =  express();
+const config        =  require("./config/config");
+const routes        =  require("./config/routes");
 
-const app = express()
+mongoose.connect(config.db);
 
-app.use(bodyParser.json())
-app.use(logger('dev'))
-app.use(errorhandler())
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(errorhandler());
 
-app.get('/', (req, res)=>{
-  res.send('hello world')
-})
-
-app.listen(3000, () => console.log('RESTful blog api app listening on port 3000!'))
+app.use('/', routes);
+app.listen(config.port, () => console.log(`Express has started on port: ${config.port}`));
